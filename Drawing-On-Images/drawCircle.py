15d361af96d44f0056f8edffea22480d4bddac01
kul <kirticse.chakma869@gmail.com>
2022-01-24 09:11:35 +0100

    Ellips file added

--- Drawing-On-Images/drawCircle.py
+++ Drawing-On-Images/drawCircle.py
@@ -6,6 +6,6 @@ color=(254,2,255)
 thickness=5
 circle_md=cv2.circle(img=images, center=center, radius=RADIOUS, color=color, thickness=thickness)
 cv2.imshow('image', circle_md)
-cv2.imwrite('D:\OPENCV-ImageProcessing\OpenCV-ImageProcessing\Drawing-On-Images\bichimuhammad.jpg',circle_md)
+cv2.imwrite('D:\OPENCV-ImageProcessing\OpenCV-ImageProcessing\Drawing-On-Images\bici-Muhammad.jpg',circle_md)
 cv2.waitKey()
 cv2.destroyAllWindows()
