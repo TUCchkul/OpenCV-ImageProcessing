bece5cb587757173a7f5e12c1f7c2ceed247a69a
kul <kirticse.chakma869@gmail.com>
2022-01-24 06:16:37 +0100

    update shape image file

--- OpenCV-Basics/shape-image.py
+++ OpenCV-Basics/shape-image.py
@@ -1,4 +1,8 @@
 import cv2
+
+#to read an image in colour format which does not include alpha channel=1, cv2.IMREAD_COLOR
+# to read an image in grayscale=0, cv2.IMREAD_GRAYSCALE
+#to read an image as it is including alpha channel=-1, cv2.IMREAD_UNCHANGED
 image=cv2.imread(filename=r'md.jpg', flags=1)
 cv2.imshow('my pic',image)
 print(image.shape)
