329f0a6f9edbd9b25f96b5d7adb8d0d9c7842901
kul <kirticse.chakma869@gmail.com>
2022-01-24 08:08:29 +0100

    update

--- Drawing-On-Images/accessingPixelValues.py
+++ Drawing-On-Images/accessingPixelValues.py
@@ -2,7 +2,8 @@ import cv2
 image=cv2.imread(r'md.jpg',flags=1)
 print(image[0:100,0:100])
 #Changes pixel value in the original images
-image[0:100,0:100]=255
+#image[0:100,0:100]=255#fully white
+image[0:100,0:100]=[165,42,42]#RGB format
 cv2.imshow('New Image',image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
