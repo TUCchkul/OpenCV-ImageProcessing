df864ce0f40022149a384561658f3eddb848fa49
kul <kirticse.chakma869@gmail.com>
2022-01-24 07:47:20 +0100

    update image segmentation file

--- Drawing-On-Images/imageSegmentation.py
+++ Drawing-On-Images/imageSegmentation.py
@@ -1,3 +1,8 @@
 import cv2
-image=cv2.imread(r'md.jpg')
-print(image)
+image=cv2.imread(filename=r'md.jpg', flags=1)
+print(image[-1])
+#new_image=cv2.resize(src=image,dsize=(0,0),fx=0.2,fy=0.1)
+#cv2.imshow("New_Image", new_image)
+#print(new_image.shape)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
