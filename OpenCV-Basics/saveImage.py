e5d670eaca533161e1a13d0860b4ed0b46f1dc82
kul <kirticse.chakma869@gmail.com>
2022-01-24 06:44:11 +0100

    write image in a directory

--- OpenCV-Basics/saveImage.py
+++ OpenCV-Basics/saveImage.py
@@ -6,7 +6,8 @@ height=500
 dimension=(width, height)
 #new_image=cv2.resize(src=image,dsize=dimension)
 #fx=width,fy=height
-new_image=cv2.resize(src=image,dsize=(0,0),fx=0.2,fy=0.1)
+new_image=cv2.resize(src=image,dsize=(0,0),fx=0.1,fy=0.1)
+cv2.imwrite(r'D:\OPENCV-ImageProcessing\OpenCV-ImageProcessing\OpenCV-Basics\new_imag.png', new_image)
 cv2.imshow("New_Image", new_image)
 print(new_image.shape)
 cv2.waitKey(0)
