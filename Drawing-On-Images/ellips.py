127e66ce069ea6c1f0ba1b111724f67ae1925b51
kul <kirticse.chakma869@gmail.com>
2022-01-24 14:31:26 +0100

    Rectangle file added

--- Drawing-On-Images/ellips.py
+++ Drawing-On-Images/ellips.py
@@ -4,7 +4,7 @@ CENTER=(100,100)
 AXES=(50,50)
 ANGLE=0
 START_ANGLE=0
-END_ANGLE=360
+END_ANGLE=180
 THICKNESS=3
 COLOR=(0,255,0)
 new_image=cv2.ellipse(img=image, center=CENTER,thickness=THICKNESS,angle=ANGLE, startAngle=START_ANGLE,endAngle=END_ANGLE,axes=AXES,color=COLOR)
