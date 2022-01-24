657bbb7412617be863c5b2e2573de7c1fb09b378
kul <kirticse.chakma869@gmail.com>
2022-01-24 08:26:53 +0100

    update draw line function

--- Drawing-On-Images/DrawLine.py
+++ Drawing-On-Images/DrawLine.py
@@ -1,9 +1,9 @@
 import cv2
 image=cv2.imread(r'md.jpg', flags=1)
-point1=(0,0)
-point2=(100,100)
-color=(0,255,0)
-thickness=5
+point1=(150,250)
+point2=(300,400)
+color=(250,255,255)
+thickness=25
 #Draw a line
 image_with_line=cv2.line(img=image,pt1=point1,pt2=point2, thickness=thickness,color=color)
 cv2.imshow('Image', image_with_line)
