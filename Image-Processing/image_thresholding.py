d77fc3f9250cabf420083ecd322c565f67c72d1e
kul <kirticse.chakma869@gmail.com>
2022-01-29 17:44:20 +0100

    binary inverse threshold added

--- Image-Processing/image_thresholding.py
+++ Image-Processing/image_thresholding.py
@@ -2,7 +2,8 @@ import cv2
 
 image=cv2.imread(r'md.jpg', 1)
 # thres binary =>pixel=>255:0
-ret, thresh=cv2.threshold(src=image,thresh=150, maxval=255, type=cv2.THRESH_BINARY)
+#thresh binary =>pixel=>0:255
+ret, thresh=cv2.threshold(src=image,thresh=150, maxval=255, type=cv2.THRESH_BINARY_INV)
 cv2.imshow('image', thresh)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
